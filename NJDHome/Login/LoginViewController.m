ec694894dbb4eaeae830793514352ea3d2fa03fa
yuan yunlong <yuanyunlong@yuandeiMac.local>
2017-09-30 16:23:41 +0800

    add login code

--- NJDHome/Login/LoginViewController.m
+++ NJDHome/Login/LoginViewController.m
@@ -10,6 +10,10 @@
 #import "AppDelegate.h"
 @interface LoginViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *userAccountTextfeild;
+@property (weak, nonatomic) IBOutlet UITextField *userPasswordTextfeild;
+@property (weak, nonatomic) IBOutlet UIView *accountBackView;
+@property (weak, nonatomic) IBOutlet UIButton *remberPassword;
 
 @end
 
@@ -18,7 +22,27 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.userPasswordTextfeild.placeholder = @"密码";
+    [self.userPasswordTextfeild setValue:[UIColor whiteColor] forKeyPath:@"_placeholderLabel.textColor"];
+    [self.userPasswordTextfeild setValue:[UIFont boldSystemFontOfSize:15] forKeyPath:@"_placeholderLabel.font"];
+    
+    self.userAccountTextfeild.placeholder = @"邮箱或手机号";
+    [self.userAccountTextfeild setValue:[UIColor whiteColor] forKeyPath:@"_placeholderLabel.textColor"];
+    [self.userAccountTextfeild setValue:[UIFont boldSystemFontOfSize:15] forKeyPath:@"_placeholderLabel.font"];
+    
+//    self.remberPassword.titleLabel.font = [UIFont systemFontOfSize:11];
+//    [self.remberPassword setTitle:@"记住密码" forState:UIControlStateNormal];
+//    [self.remberPassword setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+//    [self.remberPassword setImage:[UIImage imageNamed:@"loginRemberPassword"] forState:UIControlStateNormal];
+//    
+//    CGRect imageRect = self.remberPassword.imageView.frame;
+//    CGRect titleRect = self.remberPassword.titleLabel.frame;
+//    
+//    self.remberPassword.imageEdgeInsets = UIEdgeInsetsMake(0, titleRect.size.width + 10, 0,-titleRect.size.width);
+//    self.remberPassword.titleEdgeInsets = UIEdgeInsetsMake(0, -imageRect.size.width, 0, imageRect.size.width);
 }
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
